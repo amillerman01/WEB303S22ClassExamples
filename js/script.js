fe48258b433f63f30e7b00dc457f86b9acb338cc
Alex Millerman <90463478+amillerman01@users.noreply.github.com>
2022-05-21 13:31:13 -0400

    Example of explicitly iterating over many list items
    using the each method

--- js/script.js
+++ js/script.js
@@ -6,71 +6,77 @@ $(function () {
 
   // $("li#warm").hide(); // selects single element
 
-  let $author = $("#author");
-  let author = document.getElementById("author");
+  //   let $author = $("#author");
+  //   let author = document.getElementById("author");
 
-  // $author.hide(); // does work
-  // author.hide(); // doesn't work
+  //   // $author.hide(); // does work
+  //   // author.hide(); // doesn't work
 
-  // second list example
+  //   // second list example
 
-  // var $first = $("ul#list:first-child");
-  // $first.slideToggle(3000);
+  //   // var $first = $("ul#list:first-child");
+  //   // $first.slideToggle(3000);
 
-  // list .html() example
-  console.log("The html value of the first list item is: ", $("li").html());
+  //   // list .html() example
+  //   console.log("The html value of the first list item is: ", $("li").html());
 
-  // list .text() example
-  console.log(
-    "The text value of all the list items and their descendent is: ",
-    $("li").text()
-  );
-  console.log(
-    "The text value of the first list item is ACTUALLY: ",
-    $("li:first-child").text()
-  );
-
-  // list .html() and .text() setter examples
-  $("li").html("<span class='blue'>All Blue</span>");
-
-  $("li span").text("All Blue but different!");
-
-  // chaining example
-
-  // the old way without method chaining
-  // $("li").hide();
-  // $("li").html("<span class='blue'>All Blue</span>");
-  // $("li").slideDown(1000);
-
-  // The way to do this with method chaining
-  $("li").hide().html("<span class='blue'>All Blue</span>").slideDown(1000);
-
-  // remove example
-  $("#author").remove();
-
-  // replaceWith example
-  $("p").replaceWith("<div>Actual content</div>");
-
-  // attr method
-  console.log("Github link", $("a#github-link").attr("href"));
-  // using single attribute setter
-  //   $("a#github-link").attr(
-  //     "href",
-  //     "https://github.com/amillerman01/GitHub-Tutorial"
+  //   // list .text() example
+  //   console.log(
+  //     "The text value of all the list items and their descendent is: ",
+  //     $("li").text()
   //   );
-  //   // using object to set multiple attributes
-  //   $("a#github-link").attr({
-  //     href: "https://github.com/amillerman01/GitHub-Tutorial",
-  //     "aria-label": "A link to my github",
+  //   console.log(
+  //     "The text value of the first list item is ACTUALLY: ",
+  //     $("li:first-child").text()
+  //   );
+
+  //   // list .html() and .text() setter examples
+  //   $("li").html("<span class='blue'>All Blue</span>");
+
+  //   $("li span").text("All Blue but different!");
+
+  //   // chaining example
+
+  //   // the old way without method chaining
+  //   // $("li").hide();
+  //   // $("li").html("<span class='blue'>All Blue</span>");
+  //   // $("li").slideDown(1000);
+
+  //   // The way to do this with method chaining
+  //   $("li").hide().html("<span class='blue'>All Blue</span>").slideDown(1000);
+
+  //   // remove example
+  //   $("#author").remove();
+
+  //   // replaceWith example
+  //   $("p").replaceWith("<div>Actual content</div>");
+
+  //   // attr method
+  //   console.log("Github link", $("a#github-link").attr("href"));
+  //   // using single attribute setter
+  //   //   $("a#github-link").attr(
+  //   //     "href",
+  //   //     "https://github.com/amillerman01/GitHub-Tutorial"
+  //   //   );
+  //   //   // using object to set multiple attributes
+  //   //   $("a#github-link").attr({
+  //   //     href: "https://github.com/amillerman01/GitHub-Tutorial",
+  //   //     "aria-label": "A link to my github",
+  //   //   });
+  //   // using method to set attribute
+  //   $("a").attr("href", function (index, value) {
+  //     console.log(
+  //       "current anchor tag index: ",
+  //       index,
+  //       " and the current value of href is: ",
+  //       value
+  //     );
+  //     return "https://github.com/amillerman01/GitHub-Tutorial";
   //   });
-  // using method to set attribute
-  $("a").attr("href", function (index, value) {
-    console.log(
-      "current anchor tag index: ",
-      index,
-      " and the current value of href is: ",
-      value
-    );
-    return "https://github.com/amillerman01/GitHub-Tutorial";
+
+  // explicit iteration
+  $("ul#iterateOverThisList li").each(function () {
+    var listItemId = $(this).attr("id");
+    $(this).append(" has an id of " + listItemId);
   });
 });
