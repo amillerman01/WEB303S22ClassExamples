8c531638bb6e989392348aa24a349d16821a39ad
Alex Millerman <90463478+amillerman01@users.noreply.github.com>
2022-05-21 12:55:12 -0400

    Added the ready function, VERY IMPORTANT
    All jQuery code that is running immediately must be inside of the ready function definition

--- js/script.js
+++ js/script.js
@@ -1,46 +1,47 @@
-/* NOTE this jquery should not be here like this, 
-we are still missing a ready function */
+// $(document).ready(function () { // long version of ready method
+$(function () {
+  // early examples
+  // jQuery('li.hot').hide();
+  // $("li.hot").hide(); // shorthand version
 
-// jQuery('li.hot').hide();
-// $("li.hot").hide(); // shorthand version
+  // $("li#warm").hide(); // selects single element
 
-// $("li#warm").hide(); // selects single element
+  let $author = $("#author");
+  let author = document.getElementById("author");
 
-let $author = $("#author");
-let author = document.getElementById("author");
+  // $author.hide(); // does work
+  // author.hide(); // doesn't work
 
-// $author.hide(); // does work
-// author.hide(); // doesn't work
+  // second list example
 
-// second list example
+  // var $first = $("ul#list:first-child");
+  // $first.slideToggle(3000);
 
-// var $first = $("ul#list:first-child");
-// $first.slideToggle(3000);
+  // list .html() example
+  console.log("The html value of the first list item is: ", $("li").html());
 
-// list .html() example
-console.log("The html value of the first list item is: ", $("li").html());
+  // list .text() example
+  console.log(
+    "The text value of all the list items and their descendent is: ",
+    $("li").text()
+  );
+  console.log(
+    "The text value of the first list item is ACTUALLY: ",
+    $("li:first-child").text()
+  );
 
-// list .text() example
-console.log(
-  "The text value of all the list items and their descendent is: ",
-  $("li").text()
-);
-console.log(
-  "The text value of the first list item is ACTUALLY: ",
-  $("li:first-child").text()
-);
+  // list .html() and .text() setter examples
+  $("li").html("<span class='blue'>All Blue</span>");
 
-// list .html() and .text() setter examples
-$("li").html("<span class='blue'>All Blue</span>");
+  $("li span").text("All Blue but different!");
 
-$("li span").text("All Blue but different!");
+  // chaining example
 
-// chaining example
+  // the old way without method chaining
+  // $("li").hide();
+  // $("li").html("<span class='blue'>All Blue</span>");
+  // $("li").slideDown(1000);
 
-// the old way without method chaining
-// $("li").hide();
-// $("li").html("<span class='blue'>All Blue</span>");
-// $("li").slideDown(1000);
-
-// The way to do this with method chaining
-$("li").hide().html("<span class='blue'>All Blue</span>").slideDown(1000);
+  // The way to do this with method chaining
+  $("li").hide().html("<span class='blue'>All Blue</span>").slideDown(1000);
+});
