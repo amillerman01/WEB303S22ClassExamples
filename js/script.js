7493fd6c0cefb30978731c20995f368a9aa170bb
Alex Millerman <90463478+amillerman01@users.noreply.github.com>
2022-05-21 12:19:49 -0400

    Added an html getter example
    Note only the first item's html (and text) are returned

--- js/script.js
+++ js/script.js
@@ -2,17 +2,20 @@
 we are still missing a ready function */
 
 // jQuery('li.hot').hide();
-$('li.hot').hide();
+// $("li.hot").hide(); // shorthand version
 
-$('li#warm').hide();
+// $("li#warm").hide(); // selects single element
 
-let $author = $('#author');
+let $author = $("#author");
 let author = document.getElementById("author");
 
-$author.hide();
-// author.hide();
-
+// $author.hide(); // does work
+// author.hide(); // doesn't work
 
 // second list example
-var $first = $('ul#list:first-child');
-$first.slideToggle(3000);
+
+// var $first = $("ul#list:first-child");
+// $first.slideToggle(3000);
+
+// list .html() example
+console.log("The html value of the first list item is: ", $("li").html());
