0306b1c80eccf797e61ddf9e6acec01b5bd94d96
Alex Millerman <90463478+amillerman01@users.noreply.github.com>
2022-05-21 17:31:46 -0400

    Sample code for how to start to process more complex json data file

--- js/script.js
+++ js/script.js
@@ -17,10 +17,61 @@ function makeXHRCallForJSON() {
       let responseDataObject = JSON.parse(responseDataJSON);
       console.log("This is the Object version: ", responseDataObject);
       console.log("Location value: ", responseDataObject.location);
+      /* the data looks something like this
+      
+        responseDataObject = {
+            location: "New York",
+            date: "may 30",
+            map: "img/newyork.png"
+        }
+      */
       //   el.innerHTML = "<div>" + responseDataObject.location + "</div>";
       el.innerHTML = `<div>${responseDataObject.location}</div>
                       <div>${responseDataObject.date}</div>
                       <div><img src="${responseDataObject.map}"></div>`;
+
+      let responseDataTurnedBackToJSON = JSON.stringify(responseDataObject);
+      // need to add some way to parse the data, perhaps with DOMParser
+    }
+  };
+
+  xhr.send();
+}
+
+function makeXHRCallForJSONSecondTest() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "data/test2.json");
+
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      let el = document.getElementById("content");
+      let responseDataJSON = xhr.responseText;
+      console.log("This is the JSON response: ", responseDataJSON);
+      // responseDataJSON.location // can't do this
+      let responseDataObject = JSON.parse(responseDataJSON);
+      console.log("This is the Object version: ", responseDataObject);
+      /* the data looks something like this
+      
+        responseDataObject = [
+            {
+                location: "Austin, TX",
+                date: "may 15",
+                map: "img/austin.png"
+            },
+            {
+                location: "New York",
+                date: "may 30",
+                map: "img/newyork.png"
+            }
+        ]
+      */
+      // need to loop over responseDataObject.events, and then process each object
+
+      //   el.innerHTML = `<div>${responseDataObject.location}</div>
+      //                   <div>${responseDataObject.date}</div>
+      //                   <div><img src="${responseDataObject.map}"></div>`;
+
+      let responseDataTurnedBackToJSON = JSON.stringify(responseDataObject);
       // need to add some way to parse the data, perhaps with DOMParser
     }
   };
