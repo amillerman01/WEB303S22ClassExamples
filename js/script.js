6c3f9b79f5557fc12ae70ae9a23394c9df520546
Alex Millerman <90463478+amillerman01@users.noreply.github.com>
2022-05-21 12:00:29 -0400

    Added examples with id's and javascript variable conventions
    jQuery selected objects should be stored in variables that start with a $

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 // jQuery('li.hot').hide();
 $('li.hot').hide();
+
+$('li#warm').hide();
+
+let $author = $('#author');
+let author = document.getElementById("author");
+
+$author.hide();
+author.hide();
