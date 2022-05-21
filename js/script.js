83eb972f1f39318a5d8651f9d63a30818a049ec2
Alex Millerman <90463478+amillerman01@users.noreply.github.com>
2022-05-21 17:19:09 -0400

    Added JSON.parse to test

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(function () {
   $("button#retrieve-data").on("click", function () {
-    makeXHRCallForXML();
+    makeXHRCallForJSON();
   });
 });
 
@@ -11,8 +11,10 @@ function makeXHRCallForJSON() {
   xhr.onload = function () {
     if (xhr.status === 200) {
       let el = document.getElementById("content");
-      let responseData = xhr.responseText;
-      console.log(responseData);
+      let responseDataJSON = xhr.responseText;
+      console.log("This is the JSON response: ", responseDataJSON);
+      let responseDataObject = JSON.parse(responseDataJSON);
+      console.log("This is the Object version: ", responseDataObject);
       // need to add some way to parse the data, perhaps with DOMParser
     }
   };
