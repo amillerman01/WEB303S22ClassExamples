0f01dddd5d148621526ee8a372ed016ceac8a5e2
Alex Millerman <90463478+amillerman01@users.noreply.github.com>
2022-05-21 17:01:05 -0400

    Added a button to retrieve the xhr data

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 $(function () {
-  // code will go here
+  $("button#retrieve-data").on("click", function () {
+    makeXHRCall();
+  });
 });
 
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "data/test.html");
+function makeXHRCall() {
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "data/test.html");
 
-xhr.onload = function () {
-  if (xhr.status === 200) {
-    let el = document.getElementById("content");
-    el.innerHTML = xhr.responseText;
-  }
-};
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      let el = document.getElementById("content");
+      el.innerHTML = xhr.responseText;
+    }
+  };
 
-xhr.send();
+  xhr.send();
+}
