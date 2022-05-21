fc242dbd2a58628286c6cea8d83dd3b880521863
Alex Millerman <90463478+amillerman01@users.noreply.github.com>
2022-05-21 16:47:58 -0400

    Added the basis of our XHR commit, not working yet

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $(function () {
   // code will go here
 });
+
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "test.json");
+xhr.send("search=city");
+
+xhr.onload = function () {
+  if (xhr.status === 200) {
+    // process response
+  }
+};
