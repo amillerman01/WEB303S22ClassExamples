142bbcbd357f582a94d55ba8eafcd56ce6d4d5e7
Alex Millerman <90463478+amillerman01@users.noreply.github.com>
2022-06-08 19:02:56 -0400

    Added first class example for week 4

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
-$(function () {
-  $("button#retrieve-data").on("click", function () {});
-});
+function greetUser() {
+  return "Hello " + getName();
+}
+function getName() {
+  var name = "Molly";
+  return name;
+}
+
+var greeting = greetUser();
+console.log(greeting);
+
+// $(function () {
+//   $("button#retrieve-data").on("click", function () {});
+// });
