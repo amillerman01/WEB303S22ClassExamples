c9f08d58e7e5505fa883b9f0256cc929556662b9
Alex Millerman <amillerman01@stclaircollege.ca>
2022-06-22 19:54:30 -0400

    Added the second class example for objects using arrow functions

--- js/script.js
+++ js/script.js
@@ -2,23 +2,42 @@
 //   $("button#retrieve-data").on("click", function () {});
 // });
 
-let whiteRabbit = {};
-whiteRabbit.color = "white";
+// first example of object lecture
 
-// let whiteRabbit = {
-//   color: "white"
+// let whiteRabbit = {};
+// whiteRabbit.color = "white";
+
+// // let whiteRabbit = {
+// //   color: "white"
+// // };
+
+// whiteRabbit.speak = function (line) {
+//   console.log(`The ${this.color} rabbit says "${line}"`);
 // };
 
-whiteRabbit.speak = function (line) {
+// whiteRabbit.speak("I'm late!");
+
+// // without using the object, we can do something similar like this
+
+// var color = "grey";
+// function speak(line) {
+//   console.log(`The ${color} rabbit says "${line}"`);
+// }
+// speak("I'm less late");
+
+// second example of object lecture
+
+// this makes sure out speak method doesn’t output undefined
+
+var color = "rainbow";
+
+const speak = (line) => {
   console.log(`The ${this.color} rabbit says "${line}"`);
 };
 
-whiteRabbit.speak("I'm late!");
+let rabbit = {
+  color: "white",
+  speak: speak,
+};
 
-// without using the object, we can do something similar like this
-
-var color = "grey";
-function speak(line) {
-  console.log(`The ${color} rabbit says "${line}"`);
-}
-speak("I'm less late");
+rabbit.speak("I'm late and a different colour!");
